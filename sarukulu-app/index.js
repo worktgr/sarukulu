a97021a59c298401cde540979bc0d1cd270f97fc
worktgr <smateenonwork@gmail.com>
2025-10-01 00:24:23 +0530

    chore(app): upgrade to Expo SDK 54; add reanimated/worklets; update babel & entry imports

--- sarukulu-app/index.js
+++ sarukulu-app/index.js
@@ -1,4 +1,6 @@
-import 'react-native-gesture-handler'; // <-- must be first
+import 'react-native-gesture-handler'; // must be first
+import 'react-native-reanimated';      // keep this at the very top too
 import { registerRootComponent } from 'expo';
 import App from './App';
+
 registerRootComponent(App);
